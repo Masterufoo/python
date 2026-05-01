68b037db688a985d025d42654825ab8251f9c402
Masterufoo <masiha12100@gmail.com>
2026-05-01 18:13:59 +0330

    first commit

---
+++ New folder/op.py
@@ -0,0 +1,8 @@
+price = int(input("  item price: "))
+
+if price > 50000 :
+    print("shoma 20 darsad takhfif gereftid")
+elif  20000 < price <= 50000:
+    print("10 darsad takhfif ")
+elif price <= 20000:
+    print("no takhfif")
