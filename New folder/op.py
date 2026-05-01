56c26404cc9f5215661fd4b1a2ee4d395abdc031
Masterufoo <masiha12100@gmail.com>
2026-05-01 18:18:32 +0330

    Delete New folder directory

--- New folder/op.py
+++
@@ -1,8 +0,0 @@
-price = int(input("  item price: "))
-
-if price > 50000 :
-    print("shoma 20 darsad takhfif gereftid")
-elif  20000 < price <= 50000:
-    print("10 darsad takhfif ")
-elif price <= 20000:
-    print("no takhfif")
